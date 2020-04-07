7725ffd6b2d300562d3c11b8b5b5e23f8f33dbcc
Corentin.G <coco_gigpn@hotmail.com>
2020-04-07 21:02:57 +0200

    tagzone

--- src/fr/cocoraid/capitalismcraft/CapitalistPlayer.java
+++ src/fr/cocoraid/capitalismcraft/CapitalistPlayer.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 public class CapitalistPlayer {
 
 
+
+
     private static Map<UUID, CapitalistPlayer> capitalistPlayers = new HashMap();
     private boolean tagged;
     private boolean recentHit;
--- src/fr/cocoraid/capitalismcraft/warzone/TagDetectEvent.java
+++ src/fr/cocoraid/capitalismcraft/warzone/TagDetectEvent.java
@@ -6,16 +6,22 @@ import fr.cocoraid.capitalismcraft.utils.Utils;
 import fr.cocoraid.capitalismcraft.warzone.event.EnterSafezoneEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class TagDetectEvent implements Listener {
 
@@ -26,6 +32,23 @@ on peut taper avec la flèche depuis la zone
 Afficher un msg quand on entre dans la zone pop
 le message, vous entrez en mode pvp s’affiche tous les x temps
      */
+
+
+
+    //Call this method inside damage entity event if they are both inside the pvp area
+    // don't forget to remove the player from
+    // key = player to slow, value = last damager
+    private Map<UUID,UUID> playersToSlow = new HashMap<>();
+    public void updatePlayerToSlow(Player damager, Player damaged) {
+        //add player to slow down when leave the area
+        playersToSlow.put(damaged.getUniqueId(),damager.getUniqueId());
+        //check if the damager was not a previous damaged from this victim player
+        if(playersToSlow.containsKey(damager.getUniqueId()) && playersToSlow.get(damager.getUniqueId()).equals(damaged.getUniqueId())) {
+            playersToSlow.remove(damager.getUniqueId());
+        }
+    }
+
+
 
     private String world = "build";
 
@@ -38,10 +61,102 @@ le message, vous entrez en mode pvp s’affiche tous les x temps
 
 
 
+    //ETAPE 1
+    // Activer le pvp dans la zone pvp
+
+    //ETAPE 2
+    //Si le joueur est tagué et qu'il a un damager récent (moins de 6 secondes) et qu'il sort alors on lui afflige slowness
+
+    //ETAPE 3
+    //On supprime le tag au bout de 10 secondes
+    //On relance la task de suppression du tag en silence si le joueur est toujours tagué au bout des 10 secondes
+
+
+
+    //autoriser tous les damages, si le damaged est à l'intérieur
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void damage(EntityDamageEvent e) {
+        if(!e.getEntity().getWorld().getName().equalsIgnoreCase(world)) return;
+        if(e.getEntity() instanceof Player) {
+            Player p = (Player) e.getEntity();
+            //allow pvp
+            if(Safezone.getEnteredPVPZonePlayers().contains(p.getUniqueId())) {
+                e.setCancelled(false);
+            }
+        }
+    }
+
+
+    //Toucher quelqu'un de taggué vous taggue
+    //toucher quelqu'un dans l'arène vous taggue et tag l'autre joueur
+
+
+    public void tagPlayer(Player player, boolean silent) {
+
+        CapitalistPlayer cp = CapitalistPlayer.getCapitalistPlayer(player);
+
+        new BukkitRunnable() {
+            public void run() {
+                if (player.isOnline()) {
+                    if (cp.isTagged()) {
+                        if (cp.hasRecentHit()) {
+                            tagPlayer(player, true);
+                            return;
+                        }
+                        tagged.setRecentHit(false);
+                        tagged.setTagged(false);
+
+                        //si encore dans la zone
+                        if (Safezone.getEnteredPVPZonePlayers().contains(p.getUniqueId())) {
+
+
+                        } else {
+                            //OldSchoolPvp.getInstance().getNametag().setPvpTeam(p);
+                        }
+
+                        if (!silent) {
+                            Utils.sendActionBar(p, ChatColor.AQUA + " Vous n'êtes plus en pvp ");
+                        }
+                    }
+                }
+            }.
+
+            runTaskLater(instance, 20*20);
+        }
+    }
+
+
+    //interdir les flèches provenant de l'exterieur
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void damageOut(EntityDamageByEntityEvent e) {
+        if(!e.getEntity().getWorld().getName().equalsIgnoreCase(world)) return;
+        if(e.getEntity() instanceof Player) {
+            Player p = (Player) e.getEntity();
+            if(Safezone.getEnteredPVPZonePlayers().contains(p.getUniqueId())) {
+
+
+
+                //prevent arrow damage outside
+                if(e.getDamager() instanceof Arrow) {
+                    Arrow arrow = (Arrow) e.getDamager();
+                    if(arrow.getShooter() instanceof Player) {
+                        Player damager = (Player) arrow.getShooter();
+                        if(!Safezone.getEnteredPVPZonePlayers().contains(damager.getUniqueId()))
+                            e.setCancelled(true);
+                    }
+                }
+            }
+        }
+    }
+
     //prevent teleport when tagged
     @EventHandler(priority = EventPriority.LOWEST)
     public void teleport(PlayerTeleportEvent e) {
-
+        CapitalistPlayer cp = CapitalistPlayer.getCapitalistPlayer(e.getPlayer());
+        if(cp.isTagged()) {
+            e.getPlayer().sendMessage("§cLa téléportation n'est pas autorisée lorsque vous êtes en pvp !");
+            e.setCancelled(true);
+        }
     }
 
     /**
